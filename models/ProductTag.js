6a252b4da5561ef5e22981d15d16cf2795196bbf
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-13 14:59:46 -0700

    productTag model requirements

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', // This is the name of the table
+        key: 'id', // This is the column name of the referenced table
+        unique: false // must be set to false since multiple tags can be applied to the same product
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', // This is the name of the table
+        key: 'id', // This is the column name of the referenced table
+        unique: false
+      }
+    }
   },
   {
     sequelize,
