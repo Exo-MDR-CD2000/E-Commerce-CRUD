aab5e7562b0358b306440ba008d535680e72f16c
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-13 16:05:25 -0700

    category id route GET

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,9 +14,22 @@ router.get('/', async (req, res) => {
   }
 }); // working!
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}], // the 'through' and 'as' are not needed because Category and Product are associated directly through the Product model (or is it the ProdcutTag model?)
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
