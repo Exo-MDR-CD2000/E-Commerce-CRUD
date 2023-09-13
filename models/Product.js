f1da3cd67fcc5016552df94b3588204c90a90244
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-13 14:51:51 -0700

    product model requirements

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category', // This is the name of the table
+        key: 'id', // This is the column name of the referenced table
+        unique: false // must be set to false since a product can belong to the same category many times
+      }
+    }
   },
   {
     sequelize,
