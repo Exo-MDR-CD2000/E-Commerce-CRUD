a7634d0dbf378b0b2cde20117f09a41db6f0f00e
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-13 16:34:04 -0700

    added all tags GET method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,10 +3,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
+  try {
+    const tagData = await Tag.findAll(); // this is the same as SELECT * FROM tags;
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}); // working!
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
