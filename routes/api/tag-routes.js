07b2481766ad81da92350c6abe489fc0899f2a97
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-13 17:15:46 -0700

    added tag id GET method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -14,9 +14,22 @@ router.get('/', async (req, res) => {
   }
 }); // working!
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tag_has_product(s)' }], // the 'through' and 'as' are needed because Tag and Product are associated through the ProductTag model. // also defined in the index.js file in the models folder
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
