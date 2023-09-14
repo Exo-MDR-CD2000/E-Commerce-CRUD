3c5bcbe40abf15c8cdb12b6d5f8ce261c1faed08
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-13 17:24:01 -0700

    added tag DELETE method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,12 +42,37 @@ router.post('/', async (req, res) => {
   }
 }); // working!
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-});
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id // this should update the tag with the id that matches the id in the url
+      }
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}); // working!
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-});
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id // this should delete the tag with the id that matches the id in the url
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+}); // working!
 
 module.exports = router;
